495ca5e143f141ed6f0b504724486035d3a7219a
ma3067-AlwaysLearning <ma3067@rit.edu>
2026-04-10 18:13:53 +0400

    Revert "Merge pull request #5 from ma3067-AlwaysLearning/UserStory5"
    This reverts commit 2c913a3ebc07a3df524af34e0e6f4d6f75445667, reversing
    changes made to 2686275fa514da2b525f41fd2fe34ddf69867f08.

--- CERSystem/src/main/java/com/example/cersystem/repositories/EventRepository.java
+++ CERSystem/src/main/java/com/example/cersystem/repositories/EventRepository.java
@@ -9,20 +9,5 @@ public interface EventRepository extends JpaRepository<Event, Long> {
     List<Event> findByTitleContainingIgnoreCase(String title);
     List<Event> findByCategory(String category);
 
-    @Query("""
-        SELECT e
-        FROM Event e
-        WHERE (:keyword IS NULL OR LOWER(e.title) LIKE LOWER(CONCAT('%', :keyword, '%'))
-               OR LOWER(e.description) LIKE LOWER(CONCAT('%', :keyword, '%')))
-          AND (:category IS NULL OR LOWER(e.category) = LOWER(:category))
-          AND (:location IS NULL OR LOWER(e.location) LIKE LOWER(CONCAT('%', :location, '%')))
-          AND (:scheduledDate IS NULL OR e.scheduledDate = :scheduledDate)
-        ORDER BY e.scheduledDate ASC
-    """)
-    List<Event> searchAndFilter(
-            @Param("keyword") String keyword,
-            @Param("category") String category,
-            @Param("location") String location,
-            @Param("scheduledDate") LocalDate scheduledDate
-    );
+
 }
